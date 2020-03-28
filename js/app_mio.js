66ee64fccec94086200633db76f3fbb39cbdbfaa
saracastellino <sara.castellino0@gmail.com>
2020-03-28 18:37:04 +0000

    adds css

--- js/app_mio.js
+++ js/app_mio.js
@@ -20,21 +20,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const createCheckListItem = function (form) {
       const checkListItem = document.createElement('li');
-      checkListItem.classList.add('check-list-item');
-     
+      checkListItem.classList.add('form-wrapper');
     
-      const name = document.createElement('h2');
-      // name.textContent = form.fname.value;
+      const name = document.createElement('p');
       name.textContent = `Your name:  ${this.fname.value} ${this.lname.value}`;
       checkListItem.appendChild(name);
     
-      const email = document.createElement('h2');
+      const email = document.createElement('p');
       email.textContent = `Your email address: ${form.email.value}`;
       checkListItem.appendChild(email);
-    
+
+      const message = document.createElement('p');
+      message.textContent = `Here is why I deserve it: ${form.message.value}`
+      checkListItem.appendChild(message);
+
+
+      const fillInAgain = document.createElement('h6');
+      fillInAgain.textContent = `* Hey, psst! Win your friend a trip with you and fill in the form again, telling us why they deserve it too! *`
+      checkListItem.appendChild(fillInAgain);
+      
       return checkListItem;
     }
-  
+
   
   const handleDeleteClick = function () {
     const deleteButton = document.querySelector('#check-list');
