e06585b8cf7eb3df5906724ab00406a43c71b565
saracastellino <sara.castellino0@gmail.com>
2020-03-28 19:17:12 +0000

    adds changes

--- js/app.js
+++ js/app.js
@@ -1,41 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const newItemform = document.querySelector('#new-item-form');
-    newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+  const form = document.querySelector('#form');
+    form.addEventListener('submit', handleFormSubmit);
   
-    const deleteAllButton = document.querySelector('#delete-all');
-    deleteAllButton.addEventListener('click', handleDeleteAllClick);
+  const deleteButton = document.querySelector('#delete-button');
+    deleteButton.addEventListener('click', handleDeleteClick);
   })
   
-  const handleNewItemFormSubmit = function (event) {
+  const handleFormSubmit = function () {
     event.preventDefault();
+
+      const checkListItem = createCheckListItem(event.target);
+      const checkList = document.querySelector('#check-list');
+      checkList.appendChild(checkListItem);
+    
+      event.target.reset();
+    }
+
+    const createCheckListItem = function (form) {
+      const checkListItem = document.createElement('li');
+      checkListItem.classList.add('form-wrapper');
+    
+      const name = document.createElement('p');
+      name.textContent = `Your name:  ${this.fname.value} ${this.lname.value}`;
+      checkListItem.appendChild(name);
+    
+      const email = document.createElement('p');
+      email.textContent = `Your email address: ${form.email.value}`;
+      checkListItem.appendChild(email);
+
+      const message = document.createElement('p');
+      message.textContent = `Here is why I deserve it: ${form.message.value}`
+      checkListItem.appendChild(message);
+
+
+      const fillInAgain = document.createElement('h6');
+      fillInAgain.textContent = `* Hey, psst! Win your friend a trip with you and fill in the form again, telling us why they deserve it too! *`
+      checkListItem.appendChild(fillInAgain);
+      
+      return checkListItem;
+    }
+
   
-    const readingListItem = createReadingListItem(event.target);
-    const readingList = document.querySelector('#reading-list');
-    readingList.appendChild(readingListItem);
-  
-    event.target.reset();
-  }
-  
-  const createReadingListItem = function (form) {
-    const readingListItem = document.createElement('li');
-    readingListItem.classList.add('reading-list-item');
-  
-    const title = document.createElement('h2');
-    title.textContent = form.title.value;
-    readingListItem.appendChild(title);
-  
-    const author = document.createElement('h3');
-    author.textContent = form.author.value;
-    readingListItem.appendChild(author);
-  
-    const category = document.createElement('p');
-    category.textContent = form.category.value;
-    readingListItem.appendChild(category);
-  
-    return readingListItem;
-  }
-  
-  const handleDeleteAllClick = function (event) {
-    const readingList = document.querySelector('#reading-list');
-    readingList.innerHTML = '';
+  const handleDeleteClick = function () {
+    const deleteButton = document.querySelector('#check-list');
+    deleteButton.innerHTML = '';
   }
