a18bea2f1a5e0faa33f93ff5e1a316f37c64efb0
saracastellino <sara.castellino0@gmail.com>
2020-03-29 11:03:51 +0100

    adds indentation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   const form = document.querySelector('#form');
     form.addEventListener('submit', handleFormSubmit);
   
@@ -10,39 +9,60 @@ document.addEventListener('DOMContentLoaded', () => {
   const handleFormSubmit = function () {
     event.preventDefault();
 
-      const checkListItem = createCheckListItem(event.target);
-      const checkList = document.querySelector('#check-list');
-      checkList.appendChild(checkListItem);
+    const checkListItem = createCheckListItem(event.target);
+    const checkList = document.querySelector('#check-list');
+    checkList.appendChild(checkListItem);
     
-      event.target.reset();
-    }
+    event.target.reset();
+  }
 
     const createCheckListItem = function (form) {
-      const checkListItem = document.createElement('li');
-      checkListItem.classList.add('form-wrapper');
+    const checkListItem = document.createElement('li');
+    checkListItem.classList.add('form-wrapper');
+  
+    const name = document.createElement('p');
+    name.textContent = `Your name:  ${this.fname.value} ${this.lname.value}`;
+    checkListItem.appendChild(name);
+  
+    const email = document.createElement('p');
+    email.textContent = `Your email address: ${form.email.value}`;
+    checkListItem.appendChild(email);
+
+    const message = document.createElement('p');
+    message.textContent = `Here is why I deserve it: ${form.message.value}`
+    checkListItem.appendChild(message);
+
+
+    const fillInAgain = document.createElement('h6');
+    fillInAgain.textContent = `* Hey, psst! Win your friend a trip with you and fill in the form again, telling us why they deserve it too! *`
+    checkListItem.appendChild(fillInAgain);
     
-      const name = document.createElement('p');
-      name.textContent = `Your name:  ${this.fname.value} ${this.lname.value}`;
-      checkListItem.appendChild(name);
-    
-      const email = document.createElement('p');
-      email.textContent = `Your email address: ${form.email.value}`;
-      checkListItem.appendChild(email);
-
-      const message = document.createElement('p');
-      message.textContent = `Here is why I deserve it: ${form.message.value}`
-      checkListItem.appendChild(message);
-
-
-      const fillInAgain = document.createElement('h6');
-      fillInAgain.textContent = `* Hey, psst! Win your friend a trip with you and fill in the form again, telling us why they deserve it too! *`
-      checkListItem.appendChild(fillInAgain);
-      
-      return checkListItem;
+    return checkListItem;
     }
 
   
   const handleDeleteClick = function () {
-    const deleteButton = document.querySelector('#check-list');
-    deleteButton.innerHTML = '';
-  }
+  const deleteButton = document.querySelector('#check-list');
+  deleteButton.innerHTML = '';
+}
+
+
+
+  // ------- CREATE UL AND LI TO APPEND ------
+
+  // const handleFormSubmit = function () {
+  //   event.preventDefault();
+
+  //   const checkList = createCheckList(event.target);
+  //   const checkListItem = createCheckListItem(event.target);
+    
+  //   checkList.appendChild(checkListItem);
+  
+  //   event.target.reset();
+  //   }
+
+  //   const createcheckList = function () {
+  //     const checkList = document.createElement('ul');
+  //     name.textContent = `- Check before submitting -`;
+  //     checkList.classList.add('check-list');
+  //   }
